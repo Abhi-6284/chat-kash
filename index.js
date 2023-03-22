d77806a3b5751aedb6622c5391bfd6b118c6fb4a
Prashant Abhishek Lugun <prashantlugun@gmail.com>
2023-03-22 13:03:23 +0530

    PORT UPDATE

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 9846;
+const port = 8524;
 const http = require('http').Server(app)
 
 app.set('view engine', 'ejs')
